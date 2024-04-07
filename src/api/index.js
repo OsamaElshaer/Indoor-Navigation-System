6c67950feebd87f12b9528903f75578ea811438a
osamaelshaer <osamabinelshaer@gmail.com>
2024-04-07 04:03:30 +0200

    Merge conflict resolution

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
+const { floorRouter } = require("./routes/floor.routes");
+const { accessPointRouter } = require("./routes/accesspoint.routes");
 const { userRouter } = require("./routes/auth.routes");
 const { navigateRouter } = require("./routes/navigate.routes");
-const { accessPointRouter } = require("./routes/accessPoint.routes");
-const { floorRouter } = require("./routes/floor.routes");
 const router = require("express").Router();
 
 router.use("/users", userRouter);
